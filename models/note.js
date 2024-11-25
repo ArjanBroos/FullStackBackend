f3d9d33449210df0a8b658910d8b0682ef60c8cc
arjanbroos <arjanbroos@live.nl>
2024-11-25 19:55:02 +0100

    Restructure repo

--- models/note.js
+++ models/note.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-console.log('Connecting to', url)
-mongoose.connect(url)
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
